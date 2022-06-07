7fa926da32317e013cc7e0d6437539e98f422eac
yassa <yassatamer99@gmail.com>
2022-06-07 18:41:33 +0200

    user auth

--- myModules/user.js
+++ myModules/user.js
@@ -10,36 +10,68 @@ class user {
 
 
     #validateEmail(_email) {
-        return _email
+        return true;
     }
 
-    #validateUsername(_email) {
-        return _email
+    #validateUsername(_username) {
+        return true;
     }
 
-    #validatePassword(_email) {
-        return _email
+    #validatePassword(_password) {
+        return true;
     }
 
 
     async logIn () {
+        if (!this.#validateEmail(this.#email)
+         || !this.#validatePassword(this.#password)) {
+             throw "INVALID data";
+         }
+
+
         // get user from db
+
+        const dbConn = await connectToDb();
+        let res = await (await dbConn.query(`SELECT user_id, username FROM users WHERE email = '${this.#email}' AND password = '${this.#password}'`)).rows;
+
+        if (res.length == 0) {
+            throw "Email or password is INVALID";
+        }
+
+
+        
+
+        res = res[0];
+
+        this.#userid = res.user_id
+        this.#username = res.username
+
 
 
         // create cridentials
 
-
+        const token = jwd.sign(res, process.env.ACCESS_TOKEN_SECRET);
+        
+        return token;
 
     }
 
     async signUp () {
         // create user in db
 
+        if (!this.#validateEmail(this.#email)
+         || !this.#validatePassword(this.#password)
+         || !this.#validateUsername(this.#username)) {
+             throw "INVALID data";
+         }
+
+        const dbConn = await connectToDb();
+        await dbConn.query(`INSERT INTO users(username, email, password) VALUES('${this.#username}', '${this.#email}', '${this.#password}')`);
 
         // logIn
-        this.logIn();
+        const token = await this.logIn();
 
-
+        return token;
         
     }
 
